1319192edf01b92f22f82db37fface2c9786707f
Ben Slater <ben@overmation.com>
2016-09-30 16:21:05 +0100

    first commit

---
+++ twitter.rb
@@ -0,0 +1,10 @@
+require 'twitter'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "YOUR_CONSUMER_KEY"
+  config.consumer_secret     = "YOUR_CONSUMER_SECRET"
+  config.access_token        = "YOUR_ACCESS_TOKEN"
+  config.access_token_secret = "YOUR_ACCESS_SECRET"
+end
+
+client.update("I built a script to tweet this using One Month Ruby! https://onemonth.com/courses/one-month-ruby")
